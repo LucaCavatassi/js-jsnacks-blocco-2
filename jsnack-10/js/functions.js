7cc65a6637ccbb399950535b5a193ad82ff9b971
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-05 11:45:31 +0200

    feat:bozza

---
+++ jsnack-10/js/functions.js
@@ -0,0 +1,9 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function arrayGenerator (number) {
+    for (let i = 0; i = number; i++) {
+        console.log(i);
+    }
+}
---
+++ jsnack-10/js/script.js
@@ -0,0 +1,12 @@
+// Fai inserire un numero, che chiameremo N, all’utente.
+// Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+// Ogni volta che ne crei uno, stampalo a schermo.
+
+// Faccio inserire il numero all'utente. 
+// Dal numero dell'utente genero un numero uguale di array 
+    // L'array ha numeri casuali da 1 a 1000
+// Ogni volta che vengono creati stampa a schermo
+
+const userNum = parseInt(prompt("Dimmi un numero!")); //NUMBER
+
+arrayGenerator (userNum);
