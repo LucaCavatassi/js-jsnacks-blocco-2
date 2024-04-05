0bd2187e8d27939b7ba1bb90201067552e61ffcb
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-05 17:58:43 +0200

    fix: harder solution for IE

--- jsnack-13/js/functions.js
+++ jsnack-13/js/functions.js
@@ -1,7 +1,26 @@
-function trovaIndice (array, number) {
-    if (array.includes (number)){
-        return array.indexOf(number)
-    }else {
-        return number = -1;
+// SOLUZIONE FACILE 
+// function trovaIndice (array, number) {
+//     if (array.includes (number)){
+//         return array.indexOf(number)
+//     }else {
+//         return number = -1;
+//     }
+// }
+
+// SOLUZIONE DIFFICILE!!!
+function trovaIndice (array, number){
+    let isIn = false;
+    let index;
+    for (let i = 0; i < array.length; i++){
+        if (array[i] === number) {  
+            isIn = true
+            index = i
+        } 
+    }
+
+    if (isIn = true) {
+        return index
+    } else {
+        number = -1;
     }
 }
--- jsnack-13/js/script.js
+++ jsnack-13/js/script.js
@@ -1,6 +1,6 @@
 // TEST
 
 const array = [1, 2, 3, 4, 5];
-const number = 2;
+const number = 5;
 
 console.log(trovaIndice(array, number));
